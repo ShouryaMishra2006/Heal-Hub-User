7d75fc864a04bfc89108bf67e0a6a9013b143f11
Shourya Mishra <150536894+ShouryaMishra2006@users.noreply.github.com>
2025-06-26 06:58:55 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,9 +32,9 @@ const Navbar = () => {
         <li>
           <NavLink to="/contact" className="py-1 hover:text-primary">CONTACT</NavLink>
         </li>
-        <li>
+{/*         <li>
           <NavLink to="/pharmacist-assisstant" className="py-1 hover:text-primary">ASSISSTANT</NavLink>
-        </li>
+        </li> */}
       </ul>
 
       {/* Right Section */}
